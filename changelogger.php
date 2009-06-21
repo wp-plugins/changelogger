6fb42bb41ab550960824a8f7b75b4e8deaed70f9
Alphawolf <Alphawolf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-21 15:17:54 +0000

    Version update 1.0
    git-svn-id: https://plugins.svn.wordpress.org/changelogger/trunk@128092 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ changelogger.php
@@ -0,0 +1,243 @@
+<?php
+/**
+ * The main plugin file
+ *
+ * @package WordPress_Plugins
+ * @subpackage Changelogger
+ */
+ 
+/*
+Plugin Name: Changelogger
+Version: 1.0
+Plugin URI: http://www.schloebe.de/wordpress/changelogger-plugin/
+Description: <strong>WordPress 2.7+ only.</strong> For many many people a changelog is a very important thing; it is all about justifying to your users why they should upgrade to the latest version of a plugin. Changelogger shows the latest changelog right on the plugin listing page, whenever there's a plugin ready to be updated.
+Author: Oliver Schl&ouml;be
+Author URI: http://www.schloebe.de/
+
+
+Copyright 2009 Oliver Schlöbe (email : scripts@schloebe.de)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+/**
+ * Pre-2.6 compatibility
+ */
+if ( !defined( 'WP_CONTENT_URL' ) )
+	define( 'WP_CONTENT_URL', get_option( 'siteurl' ) . '/wp-content' );
+if ( !defined( 'WP_CONTENT_DIR' ) )
+	define( 'WP_CONTENT_DIR', ABSPATH . 'wp-content' );
+if ( !defined( 'WP_PLUGIN_URL' ) )
+	define( 'WP_PLUGIN_URL', WP_CONTENT_URL. '/plugins' );
+if ( !defined( 'WP_PLUGIN_DIR' ) )
+	define( 'WP_PLUGIN_DIR', WP_CONTENT_DIR . '/plugins' );
+
+
+/**
+ * Define the plugin version
+ */
+define("CLOS_VERSION", "1.0");
+
+/**
+ * Define the global var CLOSISWP27, returning bool if at least WP 2.7 is running
+ */
+define('CLOSISWP27', version_compare($GLOBALS['wp_version'], '2.6.999', '>'));
+
+/**
+ * Define the plugin path slug
+ */
+define("CLOS_PLUGINPATH", "/" . plugin_basename( dirname(__FILE__) ) . "/");
+
+/**
+ * Define the plugin full url
+ */
+define("CLOS_PLUGINFULLURL", WP_PLUGIN_URL . CLOS_PLUGINPATH );
+
+/**
+ * Define the plugin full directory
+ */
+define("CLOS_PLUGINFULLDIR", WP_PLUGIN_DIR . CLOS_PLUGINPATH );
+
+
+/** 
+* The Changelogger class
+*
+* @package WordPress_Plugins
+* @subpackage Changelogger
+* @since 1.0
+* @author scripts@schloebe.de
+*/
+class Changelogger {
+
+	/**
+ 	* The Changelogger class constructor
+ 	* initializing required stuff for the plugin
+ 	* 
+	* PHP 4 Compatible Constructor
+ 	*
+ 	* @since 1.0
+ 	* @author scripts@schloebe.de
+ 	*/
+	function Changelogger() {
+		$this->__construct();
+	}
+	
+	
+	/**
+ 	* The Changelogger class constructor
+ 	* initializing required stuff for the plugin
+ 	* 
+	* PHP 5 Constructor
+ 	*
+ 	* @since 1.0
+ 	* @author scripts@schloebe.de
+ 	*/		
+	function __construct(){
+		if ( !CLOSISWP27 ) {
+			add_action('admin_notices', array(&$this, 'wpVersion27Failed'));
+			return;
+		}
+		
+		if ( is_admin() ) {
+			add_action('init', array(&$this, 'load_textdomain'));
+			add_action('init', array(&$this, 'init'));
+			add_action('after_plugin_row', array(&$this, 'display_info_row'), 50, 2);
+		}
+	}
+	
+	
+	/**
+ 	* Initialize and load the plugin stuff
+ 	*
+ 	* @since 1.0
+ 	* @uses $pagenow
+ 	* @author scripts@schloebe.de
+ 	*/
+	function init() {
+		global $pagenow;
+		if ( !function_exists("add_action") ) return;
+		
+		if( $pagenow == 'plugins.php' ) {
+			add_action('admin_head', array(&$this, 'css_admin_header'));
+		}
+	}
+	
+	
+	/**
+ 	* Add a plugin row to display changelog via plugin API
+ 	*
+ 	* @since 1.0
+ 	* @author scripts@schloebe.de
+ 	*/
+	function display_info_row( $file, $plugin_data ) {
+		global $wp_version;
+		
+		$current = version_compare( $GLOBALS['wp_version'], '2.7.999', '>' ) ? get_transient( 'update_plugins' ) : get_option( 'update_plugins' );
+		if (!isset($current->response[$file])) return false;
+		
+		$r = $current->response[ $file ];	
+		include_once( ABSPATH . 'wp-admin/includes/plugin-install.php' );
+		$columns = version_compare( $GLOBALS['wp_version'], '2.7.999', '>' ) ? 3 : 5;
+		$api = plugins_api('plugin_information', array('slug' => $r->slug, 'fields' => array('tested' => false, 'requires' => false, 'rating' => false, 'downloaded' => false, 'downloadlink' => false, 'last_updated' => false, 'homepage' => false, 'tags' => false, 'sections' => true) ));
+		if ( !is_wp_error( $api ) && current_user_can('update_plugins') ) {
+			if( isset($api->sections['changelog']) ) {
+				$changelog = $api->sections['changelog'];
+				$section_exists = preg_match_all('/<h4>(.*)<\/h4>[\n|\r]{0,}<ul>(.*)<\/ul>[\w|\W]{0,}/isU', $changelog, $changelog_result);
+				if( $section_exists ) {
+					$is_active = is_plugin_active( $file );
+					$class = $is_active ? 'active' : 'inactive';
+					$class_tr = version_compare( $GLOBALS['wp_version'], '2.7.999', '>' ) ? ' class="plugin-update-tr second ' . $class . '"' : '';
+					echo '<tr' . $class_tr . '><td class="plugin-update CLOS-plugin-update" colspan="' . $columns . '"><div class="update-message CLOS-message">';
+					echo sprintf(__('What has changed in version %1$s', 'changelogger'), trim($changelog_result[0][0]));
+					echo '</div></td></tr>';
+				}
+			}
+		} else {
+			echo '<tr class="plugin-update-tr"><td colspan="' . $columns . '"><div class="update-message CLOS-message">';
+			echo sprintf(__('<strong>ERROR</strong>: %s', 'changelogger'), $api->get_error_message());
+			echo '</div></td></tr>';
+		}
+		#echo '<tr class="plugin-update-tr"><td colspan="' . $columns . '"><div class="update-message CLOS-message">';
+		#print_r( $api );
+		#echo '</div></td></tr>';
+	}
+
+
+	/**
+	 * Writes the css stuff into plugin page header needed for the plugin to look good ;)
+	 *
+	 * @since 1.0
+	 * @author scripts@schloebe.de
+	 */
+	function css_admin_header() {
+		echo '
+<style type="text/css">
+td.CLOS-plugin-update {
+	text-align: left;
+	border-top-width: 0px;
+}
+
+.plugin-update-tr div.CLOS-message {
+	font-weight: normal;
+}
+
+div.CLOS-message h4 {
+	margin: 0;
+	display: inline;
+}
+
+div.CLOS-message ul {
+	list-style-type: square;
+	margin-left: 20px;
+}
+</style>' . "\n";
+	}
+	
+	
+	/**
+ 	* Initialize and load the plugin textdomain
+ 	*
+ 	* @since 1.0
+ 	* @author scripts@schloebe.de
+ 	*/
+	function load_textdomain() {
+		if ( function_exists('load_plugin_textdomain') ) {
+			if ( !defined('WP_PLUGIN_DIR') )
+       		 	load_plugin_textdomain('changelogger', str_replace( ABSPATH, '', dirname(__FILE__) ) . '/languages');
+        	else
+        		load_plugin_textdomain('changelogger', false, dirname(plugin_basename(__FILE__)) . '/languages');
+		}
+	}
+	
+	
+	/**
+ 	* Checks for the version of WordPress,
+ 	* and adds a message to inform the user
+ 	* if required WP version is less than 2.7
+ 	*
+ 	* @since 1.0
+ 	* @author scripts@schloebe.de
+ 	*/
+	function wpVersion27Failed() {
+		echo "<div id='wpversionfailedmessage' class='error fade'><p>" . __('Changelogger requires at least WordPress 2.7!', 'changelogger') . "</p></div>";
+	}
+	
+}
+
+if ( class_exists('Changelogger') ) {
+	$Changelogger = new Changelogger();
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,56 @@
+=== Changelogger ===
+Contributors: Alphawolf
+Donate link: http://www.schloebe.de/donate/
+Tags: changelogger, changelog, plugin, plugins, version, control
+Requires at least: 2.7
+Tested up to: 2.9-rare
+Stable tag: trunk
+
+Changelogger shows the latest changelog right on the plugin listing page, whenever there's a plugin ready to be updated.
+
+== Description ==
+
+The 20th of June 2009 was the day when changelogs found their way into readme.txt files of plugins in the WordPress plugin repository. Thanks to Peter Westwood und Mdawaffe a new version of the readme.txt example file was available online that had an example of the changelog in action.
+
+For many many people a changelog is a very important thing; it is all about justifying to your users why they should upgrade to the latest version of a plugin. **Changelogger shows the latest changelog right on the plugin listing page**, whenever there's a plugin ready to be updated. 
+
+**Note:** The changelog on a plugin update is being shown if the plugin author added a changelog section based on the new [readme.txt example file](http://wordpress.org/extend/plugins/about/readme.txt "readme.txt example file") to their plugin's readme file.
+
+**Included languages:**
+
+* English
+* German (de_DE) (Thanks to me ;-))
+
+**Want update feeds, code documentation and more? Visit [extend.schloebe.de](http://extend.schloebe.de)**
+
+== Frequently Asked Questions ==
+
+= Why isn't this or that implemented to improve the plugin interface? =
+
+If you have suggestions please let me know by dropping me a line via e-mail or the wp.org forums.
+
+= Where can I get more information? =
+
+Please visit [the official website](http://www.schloebe.de/wordpress/better-plugin-compatibility-control-plugin/#english "Better Plugin Compatibility Control") for the latest information on this plugin.
+
+== Installation ==
+
+1. Download the plugin and unzip it.
+2. Upload the folder changelogger/ to your /wp-content/plugins/ folder.
+3. Activate the plugin from your WordPress admin panel.
+4. Installation finished.
+
+== Changelog ==
+
+= 1.0 =
+* Plugin released
+
+== Other Notes ==
+
+= Licence =
+
+This plugins is released under the GPL, you can use it free of charge on your personal or commercial blog.
+
+== Screenshots ==
+
+1. Changelog below plugin info
